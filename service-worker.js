e9ab3ef06ec93842c66ebba1e8955e41ef41a484
jofredogivan <jofredogivan@gmail.com>
2025-08-02 13:08:18 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,24 +3,38 @@ const urlsToCache = [
   '/',
   'index.html',
   'manifest.json',
-  'imagem_57099f.png',
-  'assets/image_987e83.png'
+  'imagem_57099f.png'
 ];
 
+// Instalando e cacheando arquivos estáticos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Ativando e limpando caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.filter(name => name !== CACHE_NAME)
+            .map(name => caches.delete(name))
+        )
+      )
+  );
+});
+
+// Interceptando fetch e respondendo com cache ou rede
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request).catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('index.html');
+        }
+      }))
   );
 });
